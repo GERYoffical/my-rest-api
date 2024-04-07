75079661c49f8adbed325ce17829b5b7be34a337
GERYoffical <164627055+GERYoffical@users.noreply.github.com>
2024-04-07 08:13:46 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+slrFPGNZ1cGuNSsP8@cluster0.qvtjcao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "derrenunghat3885@gmail.com" //email
 global.email_password = "NBC1048a" //application password email
